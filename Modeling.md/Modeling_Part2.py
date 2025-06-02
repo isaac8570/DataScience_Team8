700933341bbabe4f245551c03f0da4ad604e5253
kmj54 <kmj549735@gmail.com>
2025-06-02 16:16:08 +0900

    Add silhouette score

--- Modeling.md/Modeling_Part2.py
+++ Modeling.md/Modeling_Part2.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
+from sklearn.metrics import silhouette_score
 
 # 데이터 로드
 data = pd.read_csv("preprocessed_dataset.csv")
@@ -37,6 +38,10 @@ for k in [2, 3, 4]:
         'labels': data[f'Cluster_Label_k{k}']
     }
 
+    # Silhouette Score 계산
+    silhouette = silhouette_score(X_scaled, clusters)
+    cluster_results[k]['silhouette'] = silhouette
+
     # PCA 시각화
     pca = PCA(n_components=2)
     X_pca = pca.fit_transform(X_scaled)
@@ -56,15 +61,40 @@ for k in [2, 3, 4]:
 
     print(f"\n [K={k}] Cluster별 평균 특성 요약 :")
     print(summary)
+    print(f"K={k} Silhouette Score: {silhouette:.4f}")
+
+# Silhouette Score 시각화
+k_values = list(cluster_results.keys())
+silhouette_scores = [cluster_results[k]['silhouette'] for k in k_values]
+
+plt.figure(figsize=(6,4))
+plt.plot(k_values, silhouette_scores, marker='o', linestyle='-')
+plt.xlabel("Number of Clusters (K)")
+plt.ylabel("Silhouette Score")
+plt.title("Silhouette Scores for Different K")
+plt.xticks(k_values)
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
     
     
 # Elbow Method (최적 k 자동 추정) ----------- Github 참고해서 추가 작업 진행해봤습니다 ; 최적의 k 찾기
 inertia = []
+silhouette_scores = []
 K_range = range(1, 11)
 for k in K_range:
     kmeans = KMeans(n_clusters=k, random_state=42)
     kmeans.fit(X_scaled)
+    clusters = kmeans.predict(X_scaled)
     inertia.append(kmeans.inertia_)
+
+    # silhouette score는 k=1에서 계산이 안됨
+    if(k != 1):
+        silhouette = silhouette_score(X_scaled, clusters)
+        silhouette_scores.append(silhouette)
+    else:
+        silhouette_scores.append(np.nan)
 
 # Elbow 지점 자동 계산
 inertia_diff = np.diff(inertia)
@@ -80,6 +110,17 @@ plt.xlabel('Number of Clusters (k)')
 plt.ylabel('Inertia')
 plt.title('Elbow Method')
 plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+# Silhouette Score Plot
+plt.subplot(1, 2, 2)
+plt.plot(K_range, silhouette_scores, 'go-')
+plt.xlabel('Number of Clusters (k)')
+plt.ylabel('Silhouette Score')
+plt.title('Silhouette Score by k')
+plt.xticks(K_range)
 plt.grid(True)
 plt.tight_layout()
 plt.show()
